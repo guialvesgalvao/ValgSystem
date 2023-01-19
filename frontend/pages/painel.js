9816f186b593b057aca6b13044745a3fd994d829
Guilherme <guilhermeg1703@hotmail.com>
2023-01-19 01:03:39 -0300

    data fetching

--- frontend/pages/painel.js
+++ frontend/pages/painel.js
@@ -1,6 +1,32 @@
-export default function Painel () {
+export const getStaticProps = async () => {
+
+  const res = await fetch('http://localhost:3000/api/carros');
+  const data = await res.json();
+  
+  return {
+      props: { dados: data }
+  }
+}
+
+export default function Painel ({ dados }) {
     
+    const Contas = dados.result;
+
+    console.log(dados.result[0].codigo)
+
     return (
+        <div>
         <h1>Oi</h1>
+        {Contas.map((Contas, key) => (
+          <ul key={Contas.codigo}>
+            <h6>{Contas.nomeConta}</h6>
+            <li>{Contas.valor}</li>
+            <li>{Contas.obs}</li>
+            <li>{Contas.vencimento}</li>
+          </ul>
+        ))}
+        </div>
     )
 }
+
+//{dados.result}
